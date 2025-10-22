7ba914a9810d797cd3ff43cb847aa8be2852076f
baebeekay <baebeekay6@gmail.com>
2025-10-22 18:10:50 +0100

    Update string.js

--- track-backend/String-analyzer/src/models/string.js
+++ track-backend/String-analyzer/src/models/string.js
@@ -2,25 +2,13 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const String = sequelize.define('String', {
-  value: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  sha256Hash: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  isPalindrome: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false
-  },
-  length: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  }
+  value: { type: DataTypes.STRING, allowNull: false, unique: true },
+  sha256Hash: { type: DataTypes.STRING, allowNull: false },
+  isPalindrome: { type: DataTypes.BOOLEAN, allowNull: false },
+  length: { type: DataTypes.INTEGER, allowNull: false },
+  wordCount: { type: DataTypes.INTEGER, allowNull: false } // New field
 }, {
-  tableName: 'Strings', // Ensure case matches database
+  tableName: 'Strings',
   timestamps: true
 });
 
