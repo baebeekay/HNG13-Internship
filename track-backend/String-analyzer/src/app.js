fa028af77f9327f6a7653d5e3bb5e4695257b02f
baebeekay <baebeekay6@gmail.com>
2025-10-22 12:18:35 +0100

    Update app.js

--- track-backend/String-analyzer/src/app.js
+++ track-backend/String-analyzer/src/app.js
@@ -10,4 +10,12 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Internal server error' });
 });
 
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = { app, sequelize };
